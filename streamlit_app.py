9a215a89aa55470a97ca5b5180791096ad0e2f05
SMillerSnowflake <148798445+SMillerSnowflake@users.noreply.github.com>
2023-10-23 19:10:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,4 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows) 
 
+fruits_selected2 = streamlit.multiselect("Pick some fruits:",list(my_data_rows.index))
