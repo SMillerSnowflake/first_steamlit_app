8ece7580c5cfbf83cafee0f32633ef37930cc903
SMillerSnowflake <148798445+SMillerSnowflake@users.noreply.github.com>
2023-10-23 19:01:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit # make this a streamlit app
+lect * froimport streamlit # make this a streamlit app
 import pandas # so many pandas!!!!
 import requests # let us get requests via API calls
 import snowflake.connector
@@ -39,8 +39,8 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
